964ffa1a533c42eb9d72c032e3ecd6c834f5a811
png670 <png6760@gmail.com>
2025-02-18 15:41:01 +0000

    test

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 
 {
-  description = "DWM flake";
+  description = "DWM (Dynamic Window Manager)";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.dwm.overrideAttrs (old: {
-      src = ./.;
-    });
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "dwm";
+        src = /path/to/your/dwm;  # Use the local path to your dwm source
+
+        buildInputs = [ pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.xorg.libXinerama ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          make PREFIX=$out install
+        '';
+      };
+    };
 }
