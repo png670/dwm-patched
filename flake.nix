36cc09d996faa640673bf713df66bd408b235c7f
png670 <png6760@gmail.com>
2025-02-18 14:39:47 +0000

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,46 +1,12 @@
-{
-  description = "A very basic flake";
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    ,
-    }:
-    let
-      overlay =
-        final: prev: {
-          dwm = prev.dwm.overrideAttrs (oldAttrs: rec {
-            postPatch = (oldAttrs.postPatch or "") + ''
-              cp -r DEF/* .
-            '';
-            version = "main";
-            src = ./.;
-          });
-        };
-    in
-    flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              self.overlays.default
-            ];
-          };
-        in
-        rec {
-          packages.dwm = pkgs.dwm;
-          packages.default = pkgs.dwm;
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [ xorg.libX11 xorg.libXft xorg.libXinerama gcc ];
-          };
-        }
-      )
-    // { overlays.default = overlay; };
+{
+  description = "DWM flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.dwm.overrideAttrs (old: {
+      src = ./.;
+    });
+  };
 }
