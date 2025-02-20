f228b937c314022ab87827c54908b6a2784d8225
png670 <png6760@gmail.com>
2025-02-20 18:48:53 +0000

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              dwm = prev.dwm.overrideAttrs (oldAttrs: rec {
+                version = "master";
+                src = ./.;
+              });
+            })
+          ];
+        };
+      in
+      rec {
+        apps = {
+          dwm = {
+            type = "app";
+            program = "${defaultPackage}/bin/kitty";
+          };
+        };
+
+        packages.dwm = pkgs.dwm;
+        defaultApp = apps.dwm;
+        defaultPackage = pkgs.dwm;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ xorg.libX11 xorg.libXft xorg.libXinerama gcc bear ];
+        };
+      }
+    );
+}
